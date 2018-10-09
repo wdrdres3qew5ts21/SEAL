bef7a50116e06e081d16fa3a9a79f0284f916ad3
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-10-09 10:56:24 +0700

    [INIT] master : init microservice

---
+++ microservices/FaveriteService/src/main/java/seal/FaveriteService/FaveriteServiceApplication.java
@@ -0,0 +1,12 @@
+package seal.FaveriteService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaveriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaveriteServiceApplication.class, args);
+	}
+}
---
+++ microservices/VideoService/src/main/java/seal/VideoService/VideoServiceApplication.java
@@ -0,0 +1,12 @@
+package seal.VideoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoServiceApplication.class, args);
+	}
+}
