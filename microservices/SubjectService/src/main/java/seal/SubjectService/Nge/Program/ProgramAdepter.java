703803873f3bc6f1f4b24fc84859df307e3d1f0f
PimIsariya <pim.paw.pitode@gmail.com>
2018-10-11 11:40:41 +0700

    [UPDATE] add method getAllSubjectsByProgramId()

--- microservices/SubjectService/src/main/java/seal/SubjectService/Nge/Program/ProgramAdepter.java
+++ microservices/SubjectService/src/main/java/seal/SubjectService/Nge/Program/ProgramAdepter.java
@@ -5,13 +5,23 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import seal.SubjectService.Nge.Subject.Subject;
+
 @Service
 public class ProgramAdepter {
+    private static final String PREFIX = "https://ngelearning.sit.kmutt.ac.th/api/v0/program/";
 
     public List<Program> getAllProgramsDetail(){
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://ngelearning.sit.kmutt.ac.th/api/v0/program/";
+        String url = PREFIX;
         List<Program> programs = restTemplate.getForObject(url, List.class);
         return programs;
+    }
+
+    public List<Program> getAllSubjectsByProgramId(String programId){
+        RestTemplate restTemPlate = new RestTemplate();
+        String url = PREFIX + programId + "/subjects";
+        List<Program> subjects = restTemPlate.getForObject(url, List.class);
+        return subjects;
     }
 }
