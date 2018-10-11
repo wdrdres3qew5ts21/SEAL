c62f8f2450869cb0c2167f065f7481ac0481700c
PimIsariya <pim.paw.pitode@gmail.com>
2018-10-11 21:56:26 +0700

    [UPDATE] add @Entity anf @Id

--- microservices/SubjectService/src/main/java/seal/SubjectService/Nge/Program/Program.java
+++ microservices/SubjectService/src/main/java/seal/SubjectService/Nge/Program/Program.java
@@ -1,7 +1,12 @@
 package seal.SubjectService.Nge.Program;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Program {
 
+	@Id
     private String program_id;
     private String program_name;
     private String program_code;
