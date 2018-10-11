59228c6bc9944e6c7293ffdefe909892a3252f24
PimIsariya <pim.paw.pitode@gmail.com>
2018-10-11 11:41:41 +0700

    [UPDATE] add method getSubjectByProgramId(String program_id)

--- microservices/SubjectService/src/main/java/seal/SubjectService/Nge/Program/ProgramController.java
+++ microservices/SubjectService/src/main/java/seal/SubjectService/Nge/Program/ProgramController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,12 @@ public class ProgramController {
     public ResponseEntity<List<Program>> getPrograms() {
         List<Program> programs = programAdepter.getAllProgramsDetail();
         return new ResponseEntity<List<Program>>(programs, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/program/{program_id}/subject", method=RequestMethod.GET)
+    public ResponseEntity<List<Program>> getSubjectByProgramId(@PathVariable("program_id") String program_id){
+        List<Program> subjects = programAdepter.getAllSubjectsByProgramId(program_id);
+        return new ResponseEntity<List<Program>>(subjects, HttpStatus.OK);
     }
     
 }
