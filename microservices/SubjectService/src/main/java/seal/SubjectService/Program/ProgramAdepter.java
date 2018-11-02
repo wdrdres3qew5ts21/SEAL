449d747bbec15ca06f82ede329622f083c74f5a3
boat46450 <boat46450@hotmail.com>
2018-11-02 11:12:12 +0700

    [UPDATE] add service method for search subject

--- microservices/SubjectService/src/main/java/seal/SubjectService/Program/ProgramAdepter.java
+++ microservices/SubjectService/src/main/java/seal/SubjectService/Program/ProgramAdepter.java
@@ -1,6 +1,11 @@
 package seal.SubjectService.Program;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -25,4 +30,32 @@ public class ProgramAdepter {
         List<Subject> subjects = restTemPlate.getForObject(url, List.class);
         return subjects;
     }
+
+    public List<Subject> findSubjects(String programId, String findText) {
+        findText = findText.toLowerCase();
+        List<Subject> allSubjects = getAllSubjectsByProgramId(programId);
+        List<Subject> findSubjects = new ArrayList();
+        ObjectMapper mapper = new ObjectMapper();
+        for (int i = 0; i < allSubjects.size(); i++) {
+            Subject subject = mapper.convertValue(allSubjects.get(i), Subject.class);
+            String codeLower = subject.getCode().toLowerCase();
+            String nameLower = subject.getName().toLowerCase();
+            if (findText(codeLower, findText) || findText(nameLower, findText)) {
+                findSubjects.add(subject);
+            }
+        }
+        return findSubjects;
+    }
+
+    private boolean findText(String text, String find) {
+        int index = text.indexOf(find);
+        return index >= 0;
+    }
+
+    // private <T> List<T> jsonArrayToObjectList(String json, Class<T> tClass) throws IOException {
+    //     ObjectMapper mapper = new ObjectMapper();
+    //     CollectionType listType = mapper.getTypeFactory().constructCollectionType(ArrayList.class, tClass);
+    //     List<T> ts = mapper.readValue(json, listType);
+    //     return ts;
+    // }
 }
