7e40c5c9a262b21b2b68dc85891fb72e98f048c8
gammyp <gamebep@gmail.com>
2018-11-09 19:55:34 +0700

    [Update] Change @Getmapping to @Requestmapping

--- microservices/VideoService/src/main/java/seal/VideoService/video/VideoController.java
+++ microservices/VideoService/src/main/java/seal/VideoService/video/VideoController.java
@@ -3,11 +3,13 @@ package seal.VideoService.video;
 import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @CrossOrigin
 @RestController
@@ -16,23 +18,22 @@ public class VideoController {
     @Autowired
     private VideoAdapter videoAdapter;
 
-    @GetMapping("/video/{id}")
+    @RequestMapping(path = "/video/{id)", method = RequestMethod.GET)
     public ResponseEntity<Video> findVideoByIs(@PathVariable String id) {
-        ResponseEntity<Video> video = videoAdapter.findVideoById(id);
-        return video;
+        Video video = videoAdapter.findVideoById(id);
+        return new ResponseEntity<Video>(video, HttpStatus.OK);
     }
 
-    @GetMapping("/videos")
-    public ResponseEntity<List> findAllVideo() {
-        RestTemplate rest = new RestTemplate();
-        ResponseEntity<List> videosResponse = videoAdapter.findAllVideo();
-        return videosResponse;
+    @RequestMapping(path = "/videos", method = RequestMethod.GET)
+    public ResponseEntity<List<Video>> findAllVideo() {
+        List<Video> videosResponse = videoAdapter.findAllVideo();
+        return new ResponseEntity<List<Video>>(videosResponse, HttpStatus.OK);
     }
 
-    @GetMapping("/subject/{id}/videos")
-    public ResponseEntity<List> findVideoFromSubjectId(@PathVariable String id) {
-        ResponseEntity<List> videosFromSubjectResponse = videoAdapter.findVideoFromSubjectId(id);
-        return videosFromSubjectResponse;
+    @RequestMapping(path = "/subject/{id}/videos", method = RequestMethod.GET)
+    public ResponseEntity<List<Video>> findVideoFromSubjectId(@PathVariable String id) {
+        List<Video> videosFromSubjectResponse = videoAdapter.findVideoFromSubjectId(id);
+        return new ResponseEntity<List<Video>>(videosFromSubjectResponse, HttpStatus.OK);
     }
 
 }
