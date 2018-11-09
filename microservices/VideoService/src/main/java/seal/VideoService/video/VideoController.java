54da1bc169239781e7609a25c41ab00045c09621
gammyp <gamebep@gmail.com>
2018-11-09 16:03:48 +0700

    [UPDATE] Add crossorigin.

--- microservices/VideoService/src/main/java/seal/VideoService/video/VideoController.java
+++ microservices/VideoService/src/main/java/seal/VideoService/video/VideoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin
 @RestController
 public class VideoController {
 
