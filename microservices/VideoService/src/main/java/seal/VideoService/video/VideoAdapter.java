0837a799b34043de8b3994212970c151af1e5f55
gammyp <gamebep@gmail.com>
2018-11-09 19:54:56 +0700

    [Update] Update a Video Service.

--- microservices/VideoService/src/main/java/seal/VideoService/video/VideoAdapter.java
+++ microservices/VideoService/src/main/java/seal/VideoService/video/VideoAdapter.java
@@ -1,7 +1,6 @@
 package seal.VideoService.video;
 
 import java.util.List;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,20 +9,21 @@ public class VideoAdapter {
     private static final String VIDEO_URL = "https://ngelearning.sit.kmutt.ac.th/api/v0/video/";
     private static final String SUBJECT_URL = "https://ngelearning.sit.kmutt.ac.th/api/v0/subject/";
     
-    public ResponseEntity<Video> findVideoById(String videoID) {
+    public Video findVideoById(String videoID) {
         RestTemplate rest = new RestTemplate();
-        ResponseEntity<Video> video = rest.getForEntity(VIDEO_URL + videoID, Video.class);
+        Video video = rest.getForObject(VIDEO_URL + videoID, Video.class);
         return video;
     }
 
-    public ResponseEntity<List> findAllVideo() {
+    public List<Video> findAllVideo() {
         RestTemplate rest = new RestTemplate();
-        ResponseEntity<List> videoList = rest.getForEntity(SUBJECT_URL + "2/videos", List.class);
+        List<Video> videoList = rest.getForObject(SUBJECT_URL + "2/videos", List.class);
         return videoList;
     }
 
-    public ResponseEntity<List> findVideoFromSubjectId(String id) {
+    public List<Video> findVideoFromSubjectId(String id) {
         RestTemplate rest = new RestTemplate();
-        return rest.getForEntity(SUBJECT_URL + id + "/videos", List.class);
+        List<Video> videoList = rest.getForObject(SUBJECT_URL + id + "/videos", List.class);
+        return videoList;
     }
 }
