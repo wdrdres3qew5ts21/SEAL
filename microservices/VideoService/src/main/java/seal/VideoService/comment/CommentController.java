77c3a4b43791e6d6f425bcda4de2c55fae404501
gammyp <gamebep@gmail.com>
2018-11-09 11:20:20 +0700

    [ADD] Add a CommentController.

--- microservices/VideoService/src/main/java/seal/VideoService/comment/CommentController.java
+++ microservices/VideoService/src/main/java/seal/VideoService/comment/CommentController.java
@@ -1,5 +1,30 @@
 package seal.VideoService.comment;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CommentController {
     
+    @Autowired
+    CommentService commentService;
+    
+    @RequestMapping(path = "/comments",method = RequestMethod.GET)
+    public ResponseEntity<List<Comment>> getAllCommentInSystem() {
+        List<Comment> comment = commentService.getAllComment();
+        return new ResponseEntity<List<Comment>>(comment, HttpStatus.OK);
+    }
+    
+    @RequestMapping(path = "/comments/video/{videoId}", method = RequestMethod.GET)
+    public ResponseEntity<Comment> saveCommentFromUserToSystem(@PathVariable int videoId, @RequestBody Comment commentData) {
+        Comment commentObject = commentService.saveCommentFromController(commentData);
+        return new ResponseEntity<Comment>(commentObject,HttpStatus.OK);
+    }
 }
