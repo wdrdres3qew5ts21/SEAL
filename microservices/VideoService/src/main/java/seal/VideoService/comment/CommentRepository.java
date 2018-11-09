3489288950546046cd0c5349ae0e665b5e91ab7b
gammyp <gamebep@gmail.com>
2018-11-09 11:24:41 +0700

    [ADD] Add find comment by video id.

--- microservices/VideoService/src/main/java/seal/VideoService/comment/CommentRepository.java
+++ microservices/VideoService/src/main/java/seal/VideoService/comment/CommentRepository.java
@@ -1,9 +1,12 @@
 package seal.VideoService.comment;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Integer>{
+public interface CommentRepository extends JpaRepository<Comment, Long>{
+
+    public List<Comment> findCommentByVideoId(int videoId);
     
 }
