81eb64f24e092438d84f6ddc9c67fc290137c1cc
Unknown <wdrdres3qew5ts21@gmail.com>
2018-11-16 23:07:32 +0700

    [ADD] Exception และ Response message ให้ User Service

--- microservices/UserService/src/main/java/seal/UserService/User/UserController.java
+++ microservices/UserService/src/main/java/seal/UserService/User/UserController.java
@@ -3,6 +3,7 @@ package seal.UserService.User;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.persistence.EntityNotFoundException;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -28,6 +30,10 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    private String INCORRECT_USERID_INPUT_FORMAT = "USER ID ต้องเป็นตัวเลขรหัสนักศึกษาเท่านั้น";
+    private String NOT_FOUND_USERID = "ไม่พบ USERID ในระบบ";
+    private String NOT_FOUND_PASSWORD = "กรอก PASSWORD ผิด";
+
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUser() {
         List<User> user = userService.getAllUsers();
@@ -36,12 +42,19 @@ public class UserController {
 
     @PostMapping(path = "/user/login")
     public ResponseEntity<HashMap> signInByStudentId(@RequestBody Map<String, String> user_input, HttpServletResponse response) {
-        Long userId = Long.parseLong(user_input.get("id").toString());
-        String password = user_input.get("password").toString();
-        User user = userService.getUserById(userId);
-        System.out.println(user);
+        Long userId;
+        String password;
         HashMap<String, Object> responseData = new HashMap();
-
+        User user = null;
+        try {
+            userId = Long.parseLong(user_input.get("id").toString());
+            password = user_input.get("password").toString();
+        } catch (NumberFormatException numberFmtException) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, INCORRECT_USERID_INPUT_FORMAT);
+        } 
+        user = userService.getUserById(userId);
+        
+        System.out.println(user);
         if (user != null) {
             String userPassword = user.getPassword();
             if (userPassword.equals(password)) {
@@ -53,9 +66,10 @@ public class UserController {
                 responseData.put("jwtToken", "Bearer " + token);
                 responseData.put("user", user);
                 return new ResponseEntity<HashMap>(responseData, HttpStatus.OK);
+            } else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, NOT_FOUND_PASSWORD);
             }
         }
-        responseData.put("status", false);
-        return new ResponseEntity<HashMap>(responseData, HttpStatus.UNAUTHORIZED);
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, NOT_FOUND_USERID);
     }
 }
