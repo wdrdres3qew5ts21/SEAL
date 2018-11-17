b8d4af6ddfaddcf8aa8ffa73526f37ffaac9ca54
boat46450 <boat46450@hotmail.com>
2018-11-17 15:39:23 +0700

    [ADD] not foud exception

---
+++ microservices/SubjectService/src/main/java/seal/SubjectService/Exceptions/NotFoundException.java
@@ -0,0 +1,15 @@
+package seal.SubjectService.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+  public NotFoundException() {
+      super();
+  }
+
+  public NotFoundException (String message) {
+    super(message);
+  }
+}
