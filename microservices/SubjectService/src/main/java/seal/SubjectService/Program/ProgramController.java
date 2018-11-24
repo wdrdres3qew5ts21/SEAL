5270228542d037cac73d4b2231e5835cac4a8788
TanawatRyu <gunmag3307985@hotmail.com>
2018-11-24 15:22:56 +0700

    [EDIT] master : send programid to know what not found

--- microservices/SubjectService/src/main/java/seal/SubjectService/Program/ProgramController.java
+++ microservices/SubjectService/src/main/java/seal/SubjectService/Program/ProgramController.java
@@ -46,7 +46,7 @@ public class ProgramController {
             try {
                 subjects = programAdepter.getAllSubjectsByProgramId(program_id);
             } catch (HttpClientErrorException error) {
-                throw new NotFoundException(NOT_FOUND_MASSEGE);
+                throw new NotFoundException(program_id);
             }
         } else {
             if (find.length() == 0) {
