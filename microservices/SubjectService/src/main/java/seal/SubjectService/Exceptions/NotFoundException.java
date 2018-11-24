2eabe50719f876776733d0cc17cae0c334cc3654
TanawatRyu <gunmag3307985@hotmail.com>
2018-11-24 17:09:11 +0700

    [EDIT] master : send message to jsonlogger

--- microservices/SubjectService/src/main/java/seal/SubjectService/Exceptions/NotFoundException.java
+++ microservices/SubjectService/src/main/java/seal/SubjectService/Exceptions/NotFoundException.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import seal.SubjectService.Logger.JSONLogger;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
-  private static final Logger logger = LoggerFactory.getLogger(NotFoundException.class);
+  private JSONLogger jsonLogger; 
 
   public NotFoundException(String program_id){
-    super("Program '"+program_id+"' is Not Found!!");  
+    super("Program '"+program_id+"' is Not Found!!");
+    jsonLogger.ErrorJSONLogger("Program '"+program_id+"' is Not Found!!");;
   }
   
   public NotFoundException(String program_id, String find) {
