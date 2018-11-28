3f5d5d03a02d841ab545fadceb5f585f90c5dfd2
Unknown <patcharapol.top@mail.kmutt.ac.th>
2018-11-28 12:47:26 +0700

    [EDIT] ทำ key static ใน VideoService

--- microservices/VideoService/src/main/java/seal/VideoService/Filter/FilterConfig.java
+++ microservices/VideoService/src/main/java/seal/VideoService/Filter/FilterConfig.java
@@ -1,7 +1,6 @@
 package seal.VideoService.Filter;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
--- microservices/VideoService/src/main/java/seal/VideoService/Filter/TokenAuthenticationService.java
+++ microservices/VideoService/src/main/java/seal/VideoService/Filter/TokenAuthenticationService.java
@@ -1,48 +1,32 @@
 package seal.VideoService.Filter;
 
-import java.security.Key;
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import java.util.ArrayList;
 
+import java.util.Base64;
 import static java.util.Collections.emptyList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.logging.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import static seal.VideoService.Filter.GlobalValue.secretKey;
 
 @Service
 public class TokenAuthenticationService {
 
-    static Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-
     public static Authentication getAuthentication(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
-        System.out.println("Get Authentication : " + token);
         if (token != null) {
-                String user = Jwts.parser()
-                        .setSigningKey(SECRET_KEY)
-                        .parseClaimsJws(token.replace("Bearer ", ""))
-                        .getBody()
-                        .getSubject();
+            String user = Jwts.parser()
+                    .setSigningKey(Base64.getEncoder().encodeToString(secretKey.getBytes()))
+                    .parseClaimsJws(token.replace("Bearer ", ""))
+                    .getBody()
+                    .getSubject();
 
-            return user != null
-                    ? new UsernamePasswordAuthenticationToken(user, null, emptyList())
-                    : null;
+             return user != null
+                     ? new UsernamePasswordAuthenticationToken(user, null, emptyList())
+                     : null;
         }
         return null;
     }
