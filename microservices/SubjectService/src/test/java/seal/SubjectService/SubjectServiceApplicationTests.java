85e9fdf7485b0a36c742d2d402dac4b6a66c4ad5
PimIsariya <pim.paw.pitode@gmail.com>
2018-11-30 23:41:34 +0700

    [UPDATE] เขียนtest searchWithKeywork()

--- microservices/SubjectService/src/test/java/seal/SubjectService/SubjectServiceApplicationTests.java
+++ microservices/SubjectService/src/test/java/seal/SubjectService/SubjectServiceApplicationTests.java
@@ -1,16 +1,62 @@
 package seal.SubjectService;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import seal.SubjectService.Program.Program;
+import seal.SubjectService.Program.ProgramAdepter;
+import seal.SubjectService.Subject.Subject;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootConfiguration
+@SpringBootTest(classes=ProgramAdepter.class)
 public class SubjectServiceApplicationTests {
+        
+    @Mock
+	private ProgramAdepter programAdepter;
+
+	private List<Subject> subjects;
+	private List<Program> programs;
+
+	@Before
+	public void setUp(){
+        this.programAdepter = new ProgramAdepter();
+                
+		subjects = new ArrayList<>();
+		subjects.add(new Subject("44", "Web Programming", "INT303", ""));
+                subjects.add(new Subject("55", "Computer Programming", "INT102", ""));
+                subjects.add(new Subject("66", "Java Programming II", "INT105",""));
+
+		programs = new ArrayList<>();
+		programs.add(new Program("1", "Information Technology", "B.Sc.IT", ""));
+		programs.add(new Program("2", "Computer science", "B.Sc.CS", ""));
+	}
 
 	@Test
-	public void contextLoads() {
+	public void searchWithKeyword() throws Exception {
+		List<Subject> subjectResponse = programAdepter.findSubjects("1", "pro");
+                System.out.println("\nResult: "+programAdepter.findSubjects("1", "pro").toString()+"\n");
+		assertThat(subjectResponse.get(0).getName().toLowerCase()).contains("pro");
 	}
+
+	@Test
+	public void showAllProgram(){
+		List<Program> programResponse = programAdepter.getAllProgramsDetail();
+			
+		System.out.println("\nResult: "+programAdepter.getAllProgramsDetail().toString()+"\n");
+			
+		assertThat(programResponse.size()==2);
+	}
+        
+        
+        
+        
 
 }
