def5b35e252947d8f143410dbe63eb62d1e80ac7
Unknown <wdrdres3qew5ts21@gmail.com>
2018-12-22 20:03:38 +0700

    [FIX] แก้บั้คครั้งใหญ่ที่ทำให้แนบค่า JWT Authen ไปไมไ่ด้และ RequestParam ตอน upload

--- microservices/FileService/src/main/java/seal/FileService/controller/FileController.java
+++ microservices/FileService/src/main/java/seal/FileService/controller/FileController.java
@@ -5,11 +5,14 @@
  */
 package seal.FileService.controller;
 
+import io.jsonwebtoken.Claims;
 import seal.FileService.service.FileService;
 import java.util.HashMap;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,10 +20,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import seal.FileService.Filter.TokenAuthenticationService;
 import seal.FileService.model.SubjectFile;
 import seal.FileService.service.FileService;
 
@@ -35,8 +40,13 @@ public class FileController {
     @Autowired
     private FileService fileService;
     
+    
+    
     @PostMapping("/upload")
-    public ResponseEntity<SubjectFile> uploadFile(@RequestPart(value = "file") MultipartFile file,@ RequestPart int subjectId) {
+    public ResponseEntity<SubjectFile> uploadFile(@RequestPart(value = "file") MultipartFile file, @RequestParam(value = "subjectId") int subjectId, HttpServletRequest request) {
+        System.out.println(request.getHeader("Authorization"));
+        Claims user = TokenAuthenticationService.validateJWTAuthentication(request);
+        TokenAuthenticationService.validateIsUserRoleTeacher(user);
         return new ResponseEntity<SubjectFile>(this.fileService.uploadFile(file, subjectId), HttpStatus.CREATED);
     }
     
